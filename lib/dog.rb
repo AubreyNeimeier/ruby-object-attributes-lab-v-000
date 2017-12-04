16b8e7924f858df5978cf44ab7888a69b982b260
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-04 22:13:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@ class Dog
 
 #setter method can be used to change attributes too
   def name=(dog_name)
-    @dog_name = dog_name
+    @name = dog_name
   end
 
 #getter because it returns information stored in an instance variable
   def name
-    @dog_name
+    @name
   end
 
 end
