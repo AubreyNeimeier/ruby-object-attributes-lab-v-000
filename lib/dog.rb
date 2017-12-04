8cc2c618f8de0928ec43b45e808f999fa9c3ae2a
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-04 22:11:37 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+
+#setter
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+
+#getter because it returns information stored in an instance variable
+  def name
+    @dog_name
+  end
+
+end
