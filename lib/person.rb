71e8ac304db7c75fe3afbf9ed26ef7c927d33ace
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-04 22:20:37 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+
+#setter method assigns a innstance variable to the argument pass to it, name
+  def name=(name)
+    @name = name
+  end
+
+#getter method reads or writes the instance variable (@name). 
+  def name
+    @name
+  end
+
+  
